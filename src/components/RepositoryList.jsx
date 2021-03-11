b06b75ce468778a222098954c27a1349d1e7f42a
thefalked <giulainocrivelli@hotmail.com>
2021-03-11 00:19:20 -0400

    creating 'first' component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,52 @@
+const repositoryName = "unform";
+
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>
+        Lista de Repositórios
+      </h1>
+
+      <ul>
+        <li>
+          <strong>
+            {repositoryName}
+          </strong>
+          <p>
+            Forms in React
+          </p>
+
+          <a href="">
+            Repository Link
+          </a>
+        </li>
+
+        <li>
+          <strong>
+            {repositoryName}
+          </strong>
+          <p>
+            Forms in React
+          </p>
+
+          <a href="">
+            Repository Link
+          </a>
+        </li>
+
+        <li>
+          <strong>
+            {repositoryName}
+          </strong>
+          <p>
+            Forms in React
+          </p>
+
+          <a href="">
+            Repository Link
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
